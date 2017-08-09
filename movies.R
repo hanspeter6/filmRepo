5174041f1002b979b065ec42257469ecab8057d8
hanspeter6 <h.bakker@ru.ac.za>
2017-08-09 17:56:26 +0200

    committing movies again

--- movies.R
+++ movies.R
@@ -47,3 +47,65 @@ u1_likes
 ratings %>% filter(userId == 1 & rating > 3)
 
 #FUN arrange()
+arrange(u1_likes, desc(rating))
+arrange(u1_likes, desc(rating),timestamp)
+
+#using pipe
+u1_likes %>% arrange(desc(rating))
+
+#combining filter and arrange, using pipe
+ratings %>% filter(userId == 1 & rating > 3) %>% arrange(desc(rating))
+
+#FUN select()
+select(u1_likes,title,rating)
+#can exclude
+select(u1_likes,-userId,-timestamp)
+#using FUN everything() and ordering
+# original order
+u1_likes
+
+# reorder so title is first
+select(u1_likes, title, everything())
+
+#FUN mutate()
+mutate(u1_likes, this_is = "stupid")
+mutate(u1, like = ifelse(rating > 3, 1, 0))  
+
+#and using pipes
+ratings %>% 
+        mutate(like = ifelse(rating > 3, 1, 0)) %>%
+                filter(userId == 1) %>% 
+                        select(like, everything()) 
+
+# FUN summarise(): rows into a single row
+summarise(u1, mean = mean(rating), likes = sum(rating > 3))
+# for NAs
+# with na.rm = T
+summarise(u1, mean = mean(rating, na.rm = T), likes = sum(rating > 3, na.rm = T))
+
+# FUN groupby() : very useful to use with summarise()
+# tell dplyr to group ratings by userId
+ratings_by_user <- group_by(ratings, userId)
+
+# apply summarize() to see how many movies each user has rated
+ratings_by_user %>% summarize(count = n()) %>% head()
+
+# get sorted counts (plus some presentation stuff)
+ratings %>% 
+        group_by(userId) %>% 
+        summarize(count = n()) %>% 
+        arrange(desc(count)) %>% 
+        head(20) %>%     # take first two rows
+        t()  # transpose 
+# or with the pipe (last time)
+ratings %>% group_by(userId) %>% summarize(count = n()) %>% head(10)
+
+# using grouped grouped filters and grouped mutates
+# example of a grouped filter
+ratings %>% group_by(userId) %>% filter(rank(desc(rating)) < 2)
+
+# example of a grouped mutate
+ratings %>% 
+        group_by(userId) %>%
+        mutate(centered_rating = rating - mean(rating)) %>% 
+        select(-movieId,-timestamp,-genres)
